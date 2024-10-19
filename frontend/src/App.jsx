40df7f701aa7f1a20f2c07d2deb25f6f3bfd10fb
Maroua ALLAm <94017239+marouaalm@users.noreply.github.com>
2024-10-19 17:58:16 +0200

    feat:resolveconflit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,15 @@
 // src/App.jsx
 import React from 'react';
-<<<<<<< HEAD
 import RoutePage from './layout.jsx'; // Ensure this import matches the correct path
-=======
 import DashboardPage from './pages/DashboardPage'; // Ensure this import matches the correct path
 import BandwidthChart from './components/BandwidthChart';
->>>>>>> 86b81863da23c44eb5acddfd23ba7107ba821021
+
 
 const App = () => {
   return (
     <div className="app">
-<<<<<<< HEAD
       <RoutePage /> {/* Show only the Users Page */}
-=======
       <DashboardPage />
->>>>>>> 86b81863da23c44eb5acddfd23ba7107ba821021
     </div>
   );
 };
